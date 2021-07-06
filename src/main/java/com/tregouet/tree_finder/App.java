603dbc2c03197263b1fcd5ed38ee1c9ce1a1a7ec
Tregouet <gael.tregouet@gmail.com>
2021-07-06 16:34:22 +0200

    Add files via upload

---
+++ src/main/java/com/tregouet/tree_finder/App.java
@@ -0,0 +1,13 @@
+package com.tregouet.tree_finder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
